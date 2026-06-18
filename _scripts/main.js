ce4b5498dfdb78a02616ba666caf1133704ee167
Nikesh Balami <nikeshbalami@gmail.com>
2026-06-18 12:13:39 +0545

    add publications section, fix tagline, top-button, footer labels, skills hover
    - Add Publications section with 9 entries (reports, handbooks, op-eds)
    - Move featured projects to _data/featured-projects.yml
    - Fix intro tagline: "tech researcher" → "technology researcher"
    - Wire up existing top-button include in index.html
    - Footer text labels now always visible (removed mobile hide)
    - Skills pills: add hover fill effect
    - Add _site/ and .DS_Store to .gitignore
    - Fix broken gulp scripts task (remove invalid ESLint option)
    - Sync js/main.js with updated _scripts/main.js (remove stale toggle code)

--- _scripts/main.js
+++ _scripts/main.js
@@ -53,4 +53,5 @@ $(function() {
   sr.reveal('.experience', { viewFactor: 0.2 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
+  sr.reveal('.publications', { viewFactor: 0.05 });
 });
--- build/scripts.js
+++ build/scripts.js
@@ -1,16 +1,13 @@
 const { src, dest } = require('gulp');
-const eslint = require('gulp-eslint-new');
 const uglify = require('gulp-uglify');
- 
+
 const jsPath = '_scripts/*.js';
- 
+
 function scriptsTask() {
   return src(jsPath)
-    .pipe(eslint({ overrideConfigFile: true }))
-    .pipe(eslint.format())
     .pipe(uglify())
     .pipe(dest('_site/js'))
     .pipe(dest('js'));
 }
- 
+
 module.exports = { task: scriptsTask };
